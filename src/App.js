e747cbb9a90087bf963c30544ccb5650b7656a89
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-14 12:26:58 +0430

    init

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           <h4>{name}</h4>
           <p className='title'>{title}</p>
           <p className="text">{quote}</p>
-          <FaQuoteRight />
+          <FaQuoteRight className='icon'/>
         </article>
       })}
     </div>
