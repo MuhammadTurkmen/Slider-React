117bf24be4a57caed710580bd56cf6d5ec47d8b8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-14 12:18:09 +0430

    init

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
   const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0)
   return <h2>slider project setup</h2>;
 }
 
