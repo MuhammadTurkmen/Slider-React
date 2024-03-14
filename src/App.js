31f19ba10eda49fc3821f7beaf57e1c83ce965d5
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-14 12:17:14 +0430

    init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
+  const [people, setPeople] = useState(data)
   return <h2>slider project setup</h2>;
 }
 
