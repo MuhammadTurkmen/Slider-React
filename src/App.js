58b75da62d09e36907d2345e4822321274044ab8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-15 13:31:27 +0430

    init

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App() {
     if(index < 0) {
       setIndex(lastIndex)
     }
+    if(index > lastIndex) {
+      setIndex(0)
+    }
   }, [index, people])
   
   return <section className='section'>
