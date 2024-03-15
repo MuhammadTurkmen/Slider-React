60cc1c8c1c182980a4613d493ca43e2abb3a96e8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-15 13:00:07 +0430

    init

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         if(personIndex === index) {
           position = 'activeSlide'
         }
-        if(personIndex === index - 1 || index === 0) {
+        if(personIndex === index - 1 || (index === 0 && personIndex === people.length -1)) {
           position = 'lastSlide'
         }
         return <article className={position} key={id}>
