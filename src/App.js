b2a667af98b47edc9fa8bfdec58b0a6437bf42a5
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-15 13:03:29 +0430

    init

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
       <button className='prev' onClick={() => setIndex(index - 1)}>  
         <FiChevronLeft />
       </button> 
-      <button className='next' onClick={}>  
+      <button className='next' onClick={() => setIndex(index + 1)}>  
         <FiChevronRight />
       </button> 
     </div>
